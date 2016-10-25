c88f363be7bfff6703c29115f5e759902706e83b
trepidacious <trepidacious@gmail.com>
2016-10-25 10:36:52 +0100

    Better grid layout example

--- js/src/main/scala/org/rebeam/tree/demo/ReactGridLayoutDemo.scala
+++ js/src/main/scala/org/rebeam/tree/demo/ReactGridLayoutDemo.scala
@@ -1,37 +1,63 @@
 package org.rebeam.tree.demo
 
-import chandu0101.scalajs.react.components.materialui.MuiPaper
-import io.circe.Decoder
+import japgolly.scalajs.react.vdom.prefix_<^._
+import chandu0101.scalajs.react.components.materialui._
 import japgolly.scalajs.react._
-import org.rebeam.tree.view.ReactGridLayout
+import org.rebeam.tree.view.{ReactGridLayout, View}
 import org.rebeam.tree.view.ReactGridLayout._
+
+import scala.scalajs.js
 
 object ReactGridLayoutDemo {
 
   class Backend(scope: BackendScope[Unit, Layout]) {
+
+    val swapFirstTwo: Callback = {
+      scope.modState(l => {
+        val a = l(0)
+        val b = l(1)
+
+        val a2 = a.copy(i = b.i)
+        val b2 = b.copy(i = a.i)
+
+        List(a2, b2, l(2), l(3))
+      })
+    }
+
+    def card(key: String) =
+      <.div(^.key := key,
+        MuiCard(style = js.Dynamic.literal("height" -> "100%"))(
+//          MuiCardHeader(
+//            title = "Card " + key : ReactNode,
+//            subtitle = "Subtitle for card " + key : ReactNode
+//          )(),
+          MuiCardTitle(title = "Card " + key: ReactNode, subtitle = "Card subtitle": ReactNode)(),
+          MuiCardText(style = js.Dynamic.literal("height" -> "100%"))("Text to display on card " + key),
+          MuiCardActions(style = js.Dynamic.literal("position" -> "absolute", "bottom" -> "0px"))(
+            MuiFlatButton(label = "Swap", onTouchTap = View.touch(swapFirstTwo))()
+          )
+        )
+      )
+
     def render(props: Unit, l: Layout) = {
-      println("Layout state: " + l)
       ReactGridLayout(
         layout = l,
-        cols = 1,
-        rowHeight = 50,
+        cols = 2,
+        rowHeight = 200,
         margin = XY(20, 20),
         containerPadding = XY(0, 0),
         onLayoutChange = newLayout => scope.modState(s => newLayout)
       )(
-        MuiPaper(key = "a")("A"),
-        MuiPaper(key = "b")("B"),
-        MuiPaper(key = "c")("C"),
-        MuiPaper(key = "d")("D")
+        List("a", "b", "c", "d").map(card)
       )
     }
   }
 
   val defaultLayout = List(
-    ReactGridLayout.LayoutItem("a", 0, 3),
-    ReactGridLayout.LayoutItem("b", 0, 2),
-    ReactGridLayout.LayoutItem("c", 0, 1),
-    ReactGridLayout.LayoutItem("d", 0, 0)
+    LayoutItem("a", 0, 3),
+    LayoutItem("b", 0, 2),
+    LayoutItem("c", 0, 1),
+    LayoutItem("d", 0, 0)
   )
 
   val c = ReactComponentB[Unit]("ReactGridLayoutDemo")
