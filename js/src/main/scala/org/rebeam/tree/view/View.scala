317f00f4c81370da57e6624abd37548662e3cf46
trepidacious <trepidacious@gmail.com>
2016-11-18 15:17:12 +0000

    Simpler logic for AsStringView

--- js/src/main/scala/org/rebeam/tree/view/View.scala
+++ js/src/main/scala/org/rebeam/tree/view/View.scala
@@ -1,9 +1,9 @@
 package org.rebeam.tree.view
 
 import org.rebeam.tree.TreeUtils._
-
 import chandu0101.scalajs.react.components.Implicits._
 import chandu0101.scalajs.react.components.materialui._
+import io.circe.Encoder
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.extra.Reusability
 
@@ -61,7 +61,34 @@ object View {
     )()
   }
 
+
+  trait StringCodec[A] {
+    def format(a: A): String
+    //TODO make into an Xor with error to display as validation
+    def parse(s: String): Option[A]
+  }
+
   /**
+    * This allows for editing of values that can be represented as Strings.
+    * We use a codec that provides a mapping from some strings to model values, and
+    * from any model value to a string.
+    *
+    * We tolerate the case where there are multiple strings mapping to the same model
+    * value, and in this case we retain the user's version until the model value changes.
+    * So for example if the model values are Doubles, we permit "1." to remain in the input
+    * even if the canonical formatting of 1.0 is "1". Another example would be retaining the
+    * user's choice of case for case-insensitive input.
+    *
+    * In addition, we allow the user to edit invalid strings that do not map to a model
+    * value, in case they wish to edit (type) in a sequence that reaches a valid string via
+    * invalid ones.
+    * So for example if the model values are Doubles, we permit the user to go from "1.11" to
+    * "11.1" by first inserting an additional "." to give "1.1.1" then removing the first "." - for
+    * some people this might be the natural approach, and we shouldn't interfere.
+    *
+    * Whenever a new model value is received via props, it is used to replace the currently editing
+    * value, unless the currently editing value would parse to the same value.
+    *
     * To edit doubles properly we need to maintain state. The conversion from String to Double is many to
     * one (for example "1", "1." and "1.0" all map to 1d, and back to say "1"). This makes it impossible to
     * type "1.0" when every change to the string contents of the input is converted to a Double and back -
@@ -72,115 +99,55 @@ object View {
     * and only change it to the model's string representation if the state does NOT parse to the same double
     * as the cursor model (i.e. when the model changes in a way that is not consistent with the state).
     */
-//  val doubleView = labelledCursorView[Double]("doubleView") { p =>
-//    MuiTextField(
-//      `type` = "number",
-//      value = p.cursor.model.toString,
-//      onChange = (e: ReactEventI) => e.preventDefaultCB >> p.cursor.set(e.target.value.toDouble),
-//      floatingLabelText = p.label: ReactNode
-//    )()
-//  }
+  object AsStringView {
 
-  val doubleView = DoubleView.component
+    class Backend[A](scope: BackendScope[LabelledCursor[A], String])(implicit codec: StringCodec[A], encoder: Encoder[A]) {
 
-  object DoubleView {
-
-    def format(d: Double): String = d.toString
-    //TODO make into an Xor with error to display as validation
-    def parse(s: String): Option[Double] = s.toDoubleOpt
-
-    class Backend(scope: BackendScope[LabelledCursor[Double], Option[String]]) {
-
-      def render(props: LabelledCursor[Double], state: Option[String]) = {
-
-        val formattedProps = format(props.cursor.model)
-
-        println("Rendering...")
-
-        val text = state match {
-          // No user input yet - use props
-          case None => {
-            println("No user input, using formattedProps " + formattedProps)
-            formattedProps
-          }
-
-          // User input
-          case Some(input) => {
-            println("We have user input " + input)
-            val parsedInput = parse(input)
-            parsedInput match {
-
-              //If input can't be parsed we need to leave it alone
-              case None => {
-                println("Unparseable input so using directly")
-                input
-              }
-
-              // If input can be parsed and matches props, then use it
-              case Some(d) if d == props.cursor.model => {
-                println("Parsed input to " + d + ", matches model " + props.cursor.model)
-                input
-              }
-
-              case Some(d) => {
-                println("Parsed input to " + d + ", mismatches model " + props.cursor.model + " so using formattedProps " + formattedProps)
-                formattedProps
-              }
-
-//              // Otherwise use the props
-//              case None => {
-//                println("Couldn't parse input so using formattedProps " + formattedProps)
-//                formattedProps
-//              }
-            }
-
-          }
-        }
-
+      def render(props: LabelledCursor[A], state: String) = {
         MuiTextField(
-//          `type` = "number",
-          value = text,
+          value = state,
           onChange = (e: ReactEventI) => {
             val input = e.target.value
-            val parsed = parse(input)
-            println("onChange with input " + input + " parsed to " + parsed)
+            val parsed = codec.parse(input)
             parsed match {
-              // If user is entering invalid text, we need to let them continue,
-              // hopefully they are moving towards something parseable (e.g. for
-              // a decimal, entering a new decimal point before removing the old
-              // one). When they finish it will overwrite whatever props we have.
-              case None => {
-                println("Couldn't parse input so just setting state to Some(" + input + ") ... ")
-                e.preventDefaultCB >> scope.setState(Some(input)) >> Callback{"... set state to Some(" + input + ")"}
-              }
+              // If we have a parsed value, and it is different to cursor's model, then set model
+              case Some(d) if d != props.cursor.model => e.preventDefaultCB >> props.cursor.set(d)
 
-              // If text is valid, update the state then prop
-              case Some(d) => {
-                println("Parsed input so setting state to Some(" + input + ") then props to " + d)
-                e.preventDefaultCB >> scope.setState(Some(input)) >> props.cursor.set(d) >> Callback{"... set state to Some(" + input + ") and props to " + d}
-              }
+              // Otherwise just change state - we are editing without producing a valid new value,
+              // but we may be on the way to a valid new value
+              case _ => e.preventDefaultCB >> scope.setState(input)
             }
-
           },
           floatingLabelText = props.label: ReactNode
         )()
       }
-
     }
 
-    val component = ReactComponentB[LabelledCursor[Double]]("DoubleView")
-//      .getInitialState(scope => scope.props.cursor.model.toString)
-      .initialState(None: Option[String])
-      .backend(new Backend(_))
+    def component[A](name: String, codec: StringCodec[A])(implicit encoder: Encoder[A]) = ReactComponentB[LabelledCursor[A]](name)
+      .getInitialState[String](scope => scope.props.cursor.model.toString)
+      .backend(new Backend[A](_)(codec, encoder))
       .render(s => s.backend.render(s.props, s.state))
-      .componentWillReceiveProps(scope => {
-        println("Will receive props " + scope.nextProps.cursor.model + " so setting state to that")
-        scope.$.setState(Some(scope.nextProps.cursor.model.toString))
-      })
+      .componentWillReceiveProps(
+        scope => {
+          println("Will receive props " + scope.nextProps.cursor.model + " with state " + scope.currentState)
+          val nextModel = scope.nextProps.cursor.model
+          val currentModel = scope.currentProps.cursor.model
+          if (nextModel != currentModel && !codec.parse(scope.currentState).contains(nextModel)) {
+            scope.$.setState(codec.format(nextModel))
+          } else {
+            Callback.empty
+          }
+        }
+      )
       .build
   }
 
+  val doubleStringCodec: StringCodec[Double] = new StringCodec[Double] {
+    def format(d: Double): String = d.toString
+    def parse(s: String): Option[Double] = s.toDoubleOpt
+  }
 
+  val doubleView = AsStringView.component[Double]("DoubleView", doubleStringCodec)
 
   val booleanView = labelledCursorView[Boolean]("booleanView") { p =>
     MuiCheckbox(
