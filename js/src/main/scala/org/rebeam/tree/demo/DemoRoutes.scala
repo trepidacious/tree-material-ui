28eb73019cae22a738bff23d1eb927159c797476
trepidacious <trepidacious@gmail.com>
2016-12-12 15:11:37 +0000

    Added route to demonstrate dynamic route.

--- js/src/main/scala/org/rebeam/tree/demo/DemoRoutes.scala
+++ js/src/main/scala/org/rebeam/tree/demo/DemoRoutes.scala
@@ -1,7 +1,9 @@
 package org.rebeam.tree.demo
 
+import japgolly.scalajs.react.ReactComponentB
 import japgolly.scalajs.react.extra.router._
 import org.rebeam.tree.view.Navigation
+import japgolly.scalajs.react.vdom.prefix_<^._
 
 object DemoRoutes {
 
@@ -9,16 +11,26 @@ object DemoRoutes {
   case object Home          extends Page
   case object TodoList      extends Page
   case object Address       extends Page
+  case class ItemPage(id: Int) extends Page
 
   val title = "Tree Material UI"
 
+  val itemPage = ReactComponentB[ItemPage]("Item page")
+    .render(p => <.div(s"Info for item #${p.props.id}"))
+    .build
+
   val routerConfig = RouterConfigDsl[Page].buildConfig { dsl =>
     import dsl._
+
+    //Dymanic route can also be:
+    //      | dynamicRoute("#item" / int.caseClass[ItemPage]){case p@ItemPage(id) => p} ~> dynRender(itemPage(_))
+
 
     (trimSlashes
       | staticRoute(root,   Home) ~> render(DemoViews.homeView())
       | staticRoute("#address", Address) ~> render(DemoViews.addressView)
       | staticRoute("#todolist", TodoList) ~> render(DemoViews.todoListView)
+      | dynamicRouteCT("#item" / int.caseClass[ItemPage]) ~> dynRender(itemPage(_))
       )
 
       .notFound(redirectToPage(Home)(Redirect.Replace))
@@ -31,6 +43,7 @@ object DemoRoutes {
     "Todo List" -> TodoList,
     "Address" -> Address
   )
+
 
   val navigation = Navigation.apply[Page]
 
