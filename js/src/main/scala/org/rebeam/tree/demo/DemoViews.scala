88ceb5e2e1b02e4073b683261bf60c6620d21b63
trepidacious <trepidacious@gmail.com>
2017-01-04 13:49:18 +0000

    Navigation by clicking todo list summaries

--- js/src/main/scala/org/rebeam/tree/demo/DemoViews.scala
+++ js/src/main/scala/org/rebeam/tree/demo/DemoViews.scala
@@ -146,10 +146,18 @@ object DemoViews {
     ReactComponentB[(TodoList, Pages[TodoPage])]("todoListSummaryView")
       .render(d => {
         val list = d.props._1
+        val toList = d.props._2.set(TodoProjectListPage(list.id))
         <.div(
           ^.className := "react-sortable-item",
           SortableView.handle,
-          <.span(s"${list.id} ${list.name}")
+          <.span(
+            ^.onClick --> toList,
+            ^.cursor := "pointer",
+            s"${list.id} ${list.name}"),
+
+          MuiIconButton(
+            onTouchTap = touch(toList)
+          )(Mui.SvgIcons.NavigationChevronRight()())
         )
       })
       .build
@@ -219,65 +227,6 @@ object DemoViews {
         }
       )
     }
-  }
-
-
-  object SortableContainerDemo {
-
-    // Equivalent of ({value}) => <li>{value}</li> in original demo
-    val itemView = ReactComponentB[String]("liView")
-      .render(d => {
-        <.div(
-          ^.className := "react-sortable-item",
-          SortableView.handle,
-          <.span(s"${d.props}")
-        )
-      })
-      .build
-
-    // As in original demo
-    val sortableItem = SortableElement.wrap(itemView)
-
-    // Equivalent of the `({items}) =>` lambda passed to SortableContainer in original demo
-    val listView = ReactComponentB[List[String]]("listView")
-      .render(d => {
-        <.div(
-          ^.className := "react-sortable-list",
-          d.props.zipWithIndex.map {
-            case (value, index) =>
-              sortableItem(SortableElement.Props(index = index))(value)
-          }
-        )
-      })
-      .build
-
-    // As in original demo
-    val sortableList = SortableContainer.wrap(listView)
-
-    // As in original SortableComponent
-    class Backend(scope: BackendScope[Unit, List[String]]) {
-      def render(props: Unit, items: List[String]) = {
-        sortableList(
-          SortableContainer.Props(
-            onSortEnd = p =>
-              scope.modState(
-                l => p.updatedList(l)
-              ),
-            useDragHandle = true,
-            helperClass = "react-sortable-handler"
-          )
-        )(items)
-      }
-    }
-
-    val defaultItems = Range(0, 10).map("Item " + _).toList
-
-    val c = ReactComponentB[Unit]("SortableContainerDemo")
-      .initialState(defaultItems)
-      .backend(new Backend(_))
-      .render(s => s.backend.render(s.props, s.state))
-      .build
-
   }
 
 }
