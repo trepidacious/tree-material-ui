aef99974a0a2134b83f1ba774f1b403be71ba203
trepidacious <trepidacious@gmail.com>
2017-03-09 09:59:32 +0000

    Remove unused imports

--- js/src/main/scala/org/rebeam/tree/view/PageLayout.scala
+++ js/src/main/scala/org/rebeam/tree/view/PageLayout.scala
@@ -2,10 +2,8 @@ package org.rebeam.tree.view
 
 import chandu0101.scalajs.react.components.materialui._
 import japgolly.scalajs.react._
-import japgolly.scalajs.react.vdom.ReactTagOf
 import japgolly.scalajs.react.vdom.prefix_<^._
 import org.rebeam.tree.view.View._
-import org.scalajs.dom.html.Div
 
 import scala.scalajs.js
 
